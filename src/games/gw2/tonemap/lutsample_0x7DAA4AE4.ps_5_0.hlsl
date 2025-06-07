73df8bd07906c5d92f1f49d11509167e75f92b5c
Voosh <3669773+NotVoosh@users.noreply.github.com>
2025-06-07 06:13:07 +0200

    feat(guildwars2): add mod

---
+++ src/games/gw2/tonemap/lutsample_0x7DAA4AE4.ps_5_0.hlsl
@@ -0,0 +1,34 @@
+#include "../common.hlsl"
+
+Texture2D<float4> t2 : register(t2);
+Texture2D<float4> t1 : register(t1);
+Texture2D<float4> t0 : register(t0);
+SamplerState s2_s : register(s2);
+SamplerState s1_s : register(s1);
+SamplerState s0_s : register(s0);
+cbuffer cb0 : register(b0){
+  float4 cb0[1];
+}
+
+void main(
+  float2 v0 : TEXCOORD0,
+  float2 w0 : TEXCOORD1,
+  out float4 o0 : SV_TARGET0)
+{
+  float4 r0,r1;
+  uint4 bitmask, uiDest;
+  float4 fDest;
+
+  r0.xyz = t1.Sample(s1_s, w0.xy).xyz;
+  r0.xyz = cb0[0].xyz * r0.xyz * injectedData.fxBloom;
+  r1.xyz = applyCA(t0, s0_s, v0, injectedData.fxCA);
+  r0.xyz = r0.xyz * float3(2,2,2) + r1.xyz;
+  r0.rgb = renodx::color::srgb::DecodeSafe(r0.rgb);
+  if (!injectedData.isUnderWater) {
+    r0.rgb = applyVignette(r0.rgb, v0, injectedData.fxVignette);
+  }
+  r0.rgb = applyUserTonemap(r0.rgb, t2, s2_s);
+  o0.rgb = renodx::color::srgb::EncodeSafe(r0.rgb);
+  o0.w = 0;
+  return;
+}
