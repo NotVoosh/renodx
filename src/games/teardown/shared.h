5cc358203d8c518367a5a35e82b06e43d02393fe
Voosh <3669773+NotVoosh@users.noreply.github.com>
2025-06-07 04:59:32 +0200

    feat(teardown): add mod

---
+++ src/games/teardown/shared.h
@@ -0,0 +1,48 @@
+#ifndef SRC_TEARDOWN_SHARED_H_
+#define SRC_TEARDOWN_SHARED_H_
+
+#ifndef __cplusplus
+#include "../../shaders/renodx.hlsl"
+#endif
+
+// Must be 32bit aligned
+// Should be 4x32
+struct ShaderInjectData {
+  float toneMapType;
+  float toneMapPeakNits;
+  float toneMapGameNits;
+  float toneMapUINits;
+  float toneMapGammaCorrection;
+  float toneMapHueProcessor;
+  float toneMapHueShift;
+  float toneMapHueCorrection;
+  float toneMapPerChannel;
+  float toneMapShoulderStart;
+  float colorGradeExposure;
+  float colorGradeHighlights;
+  float colorGradeShadows;
+  float colorGradeContrast;
+  float colorGradeSaturation;
+  float colorGradeBlowout;
+  float colorGradeDechroma;
+  float colorGradeFlare;
+  float colorGradeClip;
+  float colorGradeFilter;
+  float fxBloom;
+  float fxVignette;
+  float fxSharpen;
+  float fxFilmGrain;
+  float fxFilmGrainType;
+
+  float random_1;
+  float random_2;
+  float random_3;
+};
+
+#ifndef __cplusplus
+cbuffer cb13 : register(b13, space50) {
+  ShaderInjectData injectedData : packoffset(c0);
+}
+#endif
+
+#endif  // SRC_TEARDOWN_SHARED_H_
