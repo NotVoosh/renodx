d4320ada6acd0cf0a21ac28ffa4aa3cae721a8a9
Voosh <3669773+NotVoosh@users.noreply.github.com>
2025-06-07 07:34:06 +0200

    feat(triggerhappy): add mod

---
+++ src/games/triggerhappyinteractive/totalchaos/uber_0x31271C46.ps_4_0.hlsl
@@ -0,0 +1,45 @@
+#include "../common.hlsl"
+
+Texture2D<float4> t3 : register(t3);
+Texture2D<float4> t2 : register(t2);
+Texture2D<float4> t1 : register(t1);
+Texture2D<float4> t0 : register(t0);
+SamplerState s3_s : register(s3);
+SamplerState s2_s : register(s2);
+SamplerState s1_s : register(s1);
+SamplerState s0_s : register(s0);
+cbuffer cb0 : register(b0){
+  float4 cb0[37];
+}
+
+void main(
+  float4 v0 : SV_POSITION0,
+  float2 v1 : TEXCOORD0,
+  float2 w1 : TEXCOORD1,
+  out float4 o0 : SV_Target0)
+{
+  float4 r0,r1,r2;
+  uint4 bitmask, uiDest;
+  float4 fDest;
+
+  r0.xyzw = t2.Sample(s2_s, v1.xy).xyzw;
+  r1.xyzw = t1.Sample(s1_s, w1.xy).xyzw;
+  r0.xyz = r1.xyz * r0.xxx;
+  r1.w = saturate(r1.w);
+  o0.w = r1.w;
+  r0.rgb = applyUserTonemapTC(r0.rgb, t3, s3_s, cb0[36].xyz);
+  r0.rgb = renodx::color::srgb::EncodeSafe(r0.rgb);
+  r1.xy = v1.xy * cb0[30].xy + cb0[30].zw;
+  r1.xyzw = t0.Sample(s0_s, r1.xy).xyzw;
+  r0.w = r1.w * 2 + -1;
+  r1.x = 1 + -abs(r0.w);
+  r0.w = saturate(r0.w * 3.40282347e+38 + 0.5);
+  r0.w = r0.w * 2 + -1;
+  r1.x = sqrt(r1.x);
+  r1.x = 1 + -r1.x;
+  r0.w = r1.x * r0.w;
+  r0.xyz = r0.www * float3(0.00392156886,0.00392156886,0.00392156886) * injectedData.fxNoise + r0.xyz;
+  r0.xyz = renodx::color::srgb::DecodeSafe(r0.xyz);
+  o0.xyz = PostToneMapScale(r0.xyz);
+  return;
+}
