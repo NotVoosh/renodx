5dd9385d42ba53aac30aca720a9d35604dc2122b
Voosh <3669773+NotVoosh@users.noreply.github.com>
2025-06-11 11:09:44 +0200

    feat(steelrising): hardcode RenoDRT peak, use exprolloff, remove ACES

--- src/games/steelrising/tonemap_0xA45A7649.ps_5_0.hlsl
+++ src/games/steelrising/tonemap_0xA45A7649.ps_5_0.hlsl
@@ -151,7 +151,10 @@ void main(
             r2.xyz = exp2(r2.xyz);
             r3.xyz = r2.xyz * float3(0.997784019,0.997784019,0.997784019) + float3(0.493900597,0.493900597,0.493900597);
             r2.xyz = r2.xyz / r3.xyz;
-            r1.rgb = renodx::color::srgb::EncodeSafe(injectedData.toneMapType == 0.f ? saturate(r2.rgb) : untonemapped);
+            r2.xyz = log2(r2.xyz);
+            r2.xyz = float3(0.454545468,0.454545468,0.454545468) * r2.xyz;
+            r1.xyz = exp2(r2.xyz);
+            r1.rgb = injectedData.toneMapType == 0.f ? saturate(r1.rgb) : renodx::color::gamma::EncodeSafe(untonemapped);
           } else {
             r0.w = cmp(g_ToneMapOperator == 6);
             if (r0.w != 0) {
