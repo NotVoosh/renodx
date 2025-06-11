a596b30bf09b0017816c6edf18d1097160ed71f8
Voosh <3669773+NotVoosh@users.noreply.github.com>
2025-06-11 17:32:05 +0200

    feat(guildwars2): fix flashbang

--- src/games/guildwars2/VistaFlashbang_0x616B3F61.ps_5_0.hlsl
+++ src/games/guildwars2/VistaFlashbang_0x616B3F61.ps_5_0.hlsl
@@ -15,12 +15,13 @@ void main(
   float4 fDest;
 
   r0.xyzw = t0.Sample(s0_s, v0.xy).xyzw;
+  float3 tex = r0.xyz;
   r1.x = r0.w * cb0[0].w + -cb0[1].x;
   r0.xyzw = cb0[0].xyzw * r0.xyzw;
   o0.xyzw = r0.xyzw;
   uint2 textureSize;
   t0.GetDimensions(textureSize.x, textureSize.y);
-  if (textureSize.x == 4 && textureSize.y == 4 && all(cb0[0].xyz == 1.f)) {
+  if (textureSize.x == 4 && textureSize.y == 4 && all(cb0[0].xyz == 1.f) && all(tex == 1.f)) {
     o0 *= injectedData.fxFlashbang;
   }
   if (r1.x < 0) discard;
