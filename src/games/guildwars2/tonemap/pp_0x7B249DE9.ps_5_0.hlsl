ecf1db826f249158690f6d0cd3eb966ba99dac0d
Voosh <3669773+NotVoosh@users.noreply.github.com>
2025-09-25 00:41:29 +0200

    feat(guildwars2): improve vignette behavior

--- src/games/guildwars2/tonemap/pp_0x7B249DE9.ps_5_0.hlsl
+++ src/games/guildwars2/tonemap/pp_0x7B249DE9.ps_5_0.hlsl
@@ -39,9 +39,7 @@ void main(
     r0.xyz = r0.xyz + ((r1.xyz * 2.f) / (1 + r0.xyz));
   }
   r0.xyz = renodx::color::srgb::DecodeSafe(r0.xyz);
-  if (!injectedData.isUnderWater) {
-    r0.rgb = applyVignette(r0.rgb, v0, injectedData.fxVignette);
-  }
+  r0.rgb = applyVignette(r0.rgb, v0, injectedData.fxVignette);
   r0.rgb = applyUserTonemap(r0.rgb);
   o0.rgb = renodx::color::srgb::EncodeSafe(r0.rgb);
   o0.w = 0;
